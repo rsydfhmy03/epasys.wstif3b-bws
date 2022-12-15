8da06a887739a90d3892eee2b33684f7614c1825
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-12-16 02:51:25 +0700

    alert

--- main/page/vehicle/vehicleData.php
+++ main/page/vehicle/vehicleData.php
@@ -93,7 +93,12 @@
 	<!-- <script src="../../dist/jquery-3.6.1.min.js"></script>
 	
 	<script src="../../dist/sweetalert2.all.min.js"></script>    -->
-	
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"> -->
+    </script>
+        <!-- jangan lupa menambahkan script js sweet alert di bawah ini  -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.7/dist/sweetalert2.all.min.js"></script>
         <!-- di bawah ini adalah script untuk konfirmasi hapus data dengan sweet alert  -->
         <script>
             $('.hapus').on('click', function(e){
